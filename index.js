5910e7314131aec9dc559549ba44ca7e3bd4b757
Cesar Hernandez <chema27281@gmail.com>
2022-09-25 23:06:25 -0400

    commit tree 0ea48867c3f84614dc5cc6987a886178abf29ac3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData (name,email){
+   return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com'
+        })
+    })
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const li = document.createElement('li')
+        li.innerHTML = object.id
+        document.body.appendChild(li)
+       //console.log(error.message)
+    
+    
+      })
+      .catch(function ( error ) {
+        document.body.innerHTML= error.message
+       alert('Unauthorized Access')
+        console.log(error.message)
+        })}
+    
+        
+//const li = document.createElement('li')
+        //li.innerHTML = error
+        //document.body.appendChild(li)
+       //console.log(error.message)
+    
